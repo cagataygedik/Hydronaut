6749f03d261f78df912dd7b7a734d70e0efa2827
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-02-27 16:29:59 +0300

    Developed the Label stack view

--- Hydronaut/Controllers/DrinkWaterViewController.swift
+++ Hydronaut/Controllers/DrinkWaterViewController.swift
@@ -6,14 +6,93 @@
 //
 
 import UIKit
+import SnapKit
 
 final class DrinkWaterViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    private let baseLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Well done!\nThe amount you drink today is"
+        label.font = .systemFont(ofSize: 25)
+        label.textColor = .white
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let waterCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0ml" //will remove
+        label.font = .systemFont(ofSize: 25, weight: .bold)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let achievementRateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Progress: 0%" //will remove
+        label.font = .systemFont(ofSize: 16, weight: .thin)
+        label.textColor = .white
+        return label
+    }()
+    
+    private let tapGestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer()
 
+    //MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = ThemeColor.primaryColor
+        setupView()
+        setupTapGesture()
+        setupNavigationBar()
+        setupLabelStackView()
     }
-
-
+    
+    //MARK: - Layout
+    
+    private func setupView() {
+        view.backgroundColor = ThemeColor.primaryColor
+        view.isUserInteractionEnabled = true
+    }
+    
+    private func setupTapGesture() {
+        view.addGestureRecognizer(tapGestureRecognizer)
+        tapGestureRecognizer.addTarget(self, action: #selector(didTap))
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .plain, target: self, action: #selector(resetWaterCountToZero))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person"), style: .plain, target: self, action: #selector(goToProfile))
+    }
+    
+    private func setupLabelStackView() {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        //        stackView.spacing = 2
+        
+        view.addSubview(stackView)
+        
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.left.equalTo(view.safeAreaLayoutGuide).offset(40)
+        }
+        
+        stackView.addArrangedSubview(baseLabel)
+        stackView.addArrangedSubview(waterCountLabel)
+        stackView.addArrangedSubview(achievementRateLabel)
+        
+    }
+    
+    // MARK: - Action
+    
+    @objc private func didTap() {
+        view.endEditing(true)
+    }
+    
+    @objc private func resetWaterCountToZero() {}
+    
+    @objc private func goToProfile() {}
 }
 
