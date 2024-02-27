67890e9c4824f10c922666d414012c872227cf7d
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-02-27 15:04:46 +0300

    Testing main theme color(maybe I'll change in the future)

---
+++ Hydronaut/Constants/ThemeColor.swift
@@ -0,0 +1,12 @@
+//
+//  ThemeColor.swift
+//  Hydronaut
+//
+//  Created by Celil Çağatay Gedik on 27.02.2024.
+//
+
+import UIKit
+
+struct ThemeColor {
+    static let primaryColor = UIColor(named: "ThemeColor")
+}
--- Hydronaut/Controllers/DrinkWaterViewController.swift
+++ Hydronaut/Controllers/DrinkWaterViewController.swift
@@ -11,7 +11,7 @@ final class DrinkWaterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.red
+        view.backgroundColor = ThemeColor.primaryColor
     }
 
 
