7c9dec0b62002b5554a9beda45f6393edb1cfd98
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-02-27 16:29:41 +0300

    setup the navigation bar

--- Hydronaut/Controllers/MainNavigationController.swift
+++ Hydronaut/Controllers/MainNavigationController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 final class MainNavigationController: UINavigationController {
-
+    
+    override init(rootViewController: UIViewController) {
+        super.init(nibName: nil, bundle: nil)
+        setViewControllers([rootViewController], animated: true)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
+        navigationBar.barTintColor = ThemeColor.primaryColor
+        navigationBar.tintColor = UIColor.white
+        navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
     }
 }
