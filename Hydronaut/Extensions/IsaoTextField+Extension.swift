8e41ef23ad40d0b7e3953e7263948d8a4dbfd699
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-03-02 11:35:22 +0300

    Created new extension for IsaoTextField

---
+++ Hydronaut/Extensions/IsaoTextField+Extension.swift
@@ -0,0 +1,32 @@
+//
+//  IsaoTextField+Extension.swift
+//  Hydronaut
+//
+//  Created by Celil Çağatay Gedik on 2.03.2024.
+//
+
+import UIKit
+import TextFieldEffects
+
+extension IsaoTextField {
+    static func initCustomizedField(keyboardType type: UIKeyboardType = .default) -> IsaoTextField {
+        let textField = IsaoTextField.init()
+        textField.keyboardType = type
+        textField.activeColor = .white
+        textField.inactiveColor = HYDColors.inactiveColor
+        textField.adjustsFontSizeToFitWidth = true
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.textColor = .white
+        textField.tintColor = .white
+        return textField
+    }
+}
+
+extension IsaoTextField {
+    open override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+        if action == #selector(UIResponderStandardEditActions.paste(_:)) {
+            return false
+        }
+        return super.canPerformAction(action, withSender: sender)
+    }
+}
