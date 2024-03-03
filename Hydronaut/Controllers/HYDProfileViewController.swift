41c504059d3ed4ae18e8c4a8b3d6ecedead69e95
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-03-03 22:26:35 +0300

    Setup the ProfileViewController and developed image view

--- Hydronaut/Controllers/HYDProfileViewController.swift
+++ Hydronaut/Controllers/HYDProfileViewController.swift
@@ -6,11 +6,54 @@
 //
 
 import UIKit
+import TextFieldEffects
 
 final class HYDProfileViewController: UIViewController {
-
+    
+    //MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "ImageTest"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let tapGestureRecognizer: UITapGestureRecognizer = UITapGestureRecognizer()
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupView()
+        setupTapGesture()
+        setupNavigationBar()
+        setupProfileImageView()
     }
+    
+    //MARK: - Layout
+    private func setupView() {
+        view.backgroundColor = HYDColors.primaryColor
+    }
+    
+    private func setupTapGesture() {
+        view.addGestureRecognizer(tapGestureRecognizer)
+        tapGestureRecognizer.addTarget(self, action: #selector(didTap))
+    }
+    
+    private func setupNavigationBar() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveButtonTapped))
+    }
+    
+    private func setupProfileImageView() {
+        view.addSubview(profileImageView)
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(40)
+            make.leading.equalTo(view).offset(125)
+            make.trailing.equalTo(view).offset(-125)
+            make.height.equalTo(profileImageView.snp.width)
+        }
+    }
+    
+    //MARK: - Actions
+    @objc private func saveButtonTapped() {}
+    
+    @objc private func didTap() { view.endEditing(true) }
 }
