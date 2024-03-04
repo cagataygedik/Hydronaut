8a6641559152005b44602e05a3c947e7acbc21fe
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-03-04 22:58:57 +0300

    Developed the text fields of the Profile view

--- Hydronaut/Views/HYDProfileView.swift
+++ Hydronaut/Views/HYDProfileView.swift
@@ -17,10 +17,27 @@ final class HYDProfileView: UIView {
         return imageView
     }()
     
+    private let nameTextField: IsaoTextField = IsaoTextField.initCustomizedField()
+    private let heightTextField: IsaoTextField = IsaoTextField.initCustomizedField(keyboardType: .numberPad)
+    private let weightTextField: IsaoTextField = IsaoTextField.initCustomizedField(keyboardType: .numberPad)
+    
+    private var arrangeSubviews: [UIStackView] = []
+    
+    private lazy var mainVerticalStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: arrangeSubviews)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        return stackView
+    }()
+    
+    //MARK: - Life Cycle
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupView()
         setupProfileImageView()
+        setupMainStackView()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -41,5 +58,43 @@ final class HYDProfileView: UIView {
             make.trailing.equalToSuperview().offset(-125)
             make.height.equalTo(profileImageView.snp.width)
         }
+    }
+    
+    private func arrangeVerticalStackViews() {
+        let nameVerticalStackView = makeConfiguredStackView(title: "Please set a nickname", textField: nameTextField)
+        arrangeSubviews.append(nameVerticalStackView)
+        
+        let heightVerticalStackView = makeConfiguredStackView(title: "Please set your height (cm)", textField: heightTextField)
+        arrangeSubviews.append(heightVerticalStackView)
+        
+        let weightVerticalStackView = makeConfiguredStackView(title: "Please enter your weight (kg)", textField: weightTextField)
+        arrangeSubviews.append(weightVerticalStackView)
+    }
+    
+    private func setupMainStackView() {
+        arrangeVerticalStackViews()
+        
+        addSubview(mainVerticalStackView)
+        mainVerticalStackView.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(40)
+            make.leading.equalToSuperview().offset(60)
+            make.trailing.equalToSuperview().offset(-60)
+            make.height.greaterThanOrEqualTo(0)
+        }
+    }
+}
+//MARK: - Extensions
+
+extension HYDProfileView {
+    private func makeConfiguredStackView(title: String, textField: UITextField) -> UIStackView {
+        let label = UILabel()
+        label.text = title
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = UIColor.white
+        
+        let stackView = UIStackView(arrangedSubviews: [label, textField])
+        stackView.axis = .vertical
+        stackView.spacing = 5
+        return stackView
     }
 }
