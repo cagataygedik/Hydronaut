fcf83612002732542d692fb05954137b8a119ede
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-04-28 13:33:08 +0300

    Ready for launch... I guess?

--- Hydronaut/CustomViews/HYDTextField.swift
+++ Hydronaut/CustomViews/HYDTextField.swift
@@ -28,23 +28,44 @@ final class HYDTextField: UITextField {
         keyboardType = .numberPad
         clearButtonMode = .whileEditing
         let toolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: 36))
-                toolBar.barStyle = .default
-                toolBar.sizeToFit()
+        toolBar.barStyle = .default
+        toolBar.sizeToFit()
         let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneButtonTapped))
-                toolBar.items = [UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil,action: nil),
-                                 doneButton
-                ]
-                toolBar.isUserInteractionEnabled = true
-                inputAccessoryView = toolBar
+        toolBar.items = [UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil,action: nil),
+                         doneButton
+        ]
+        toolBar.isUserInteractionEnabled = true
+        inputAccessoryView = toolBar
     }
     
     @objc private func doneButtonTapped() {
-        let waterVolumeString = (text?.replacingOccurrences(of: "ml", with: ""))!
+        guard let waterVolumeString = text?.replacingOccurrences(of: "ml", with: ""), !waterVolumeString.isEmpty else {
+            let alert = UIAlertController(title: "😳 Oopsie 😳", message: "Please enter a value first", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "I'm on it", style: .default, handler: nil))
+            if let currentViewController = UIApplication.shared.windows.first?.rootViewController {
+                currentViewController.present(alert, animated: true, completion: nil)
+            }
+            return
+        }
+        
+        if WaterManager.shared.recommendedIntake == 0 {
+            let alert = UIAlertController(title: "🤔 No user info 🤔", message: "Please enter your info first", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+                if let navigationController = UIApplication.shared.windows.first?.rootViewController as? UINavigationController {
+                    navigationController.pushViewController(HYDProfileViewController(), animated: true)
+                }
+            }))
+            if let currentViewController = UIApplication.shared.windows.first?.rootViewController {
+                currentViewController.present(alert, animated: true, completion: nil)
+            }
+            return
+        }
+        
         let waterVolume = Int(waterVolumeString) ?? 0
         WaterManager.shared.addWaterVolume(size: waterVolume)
         text = ""
         endEditing(true)
-        }
+    }
 }
 //Disables paste action from the text field.
 extension HYDTextField {
